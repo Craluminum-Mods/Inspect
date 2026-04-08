4f4531050f7b01a9da0d8d752030b1784504a319
Dana <69315569+Craluminum2413@users.noreply.github.com>
2026-04-08 14:13:03 +0300

    allow to inspect any normal slot as well

---
+++ Inspect/HarmonyPatches/GuiElementItemSlotGridBase_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+namespace Inspect.HarmonyPatches;
+
+[HarmonyPatch(typeof(GuiElementItemSlotGridBase), nameof(GuiElementItemSlotGridBase.RenderInteractiveElements))]
+public static class GuiElementItemSlotGridBase_Patch
+{
+    [HarmonyPostfix]
+    public static void Postfix(GuiElementItemSlotGridBase __instance, float deltaTime, ICoreClientAPI ___api, IInventory ___inventory)
+    {
+        if (GuiDialogInspect.LockStack) return;
+
+        if (__instance.hoverSlotId != -1 && ___inventory[__instance.hoverSlotId] != null)
+        {
+            GuiDialogInspect.SetStack(___inventory[__instance.hoverSlotId]?.Itemstack?.Clone());
+        }
+    }
+}
